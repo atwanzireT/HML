88c07000af6dba22b25681d627b4c6f2f65de07a
atwanzireT <tatwanzire@gmail.com>
2022-11-06 20:20:44 +0300

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+import uuid
 
 # Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to = "services")
+    description = RichTextField(blank = True)
+
+    def __str__(self):
+        return self.title
+
+class Room(models.Model):
+    title = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to = 'rooms')
+    description = RichTextField(blank = True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.description
+
+class Booking(models.Model):
+    booking_id = models.UUIDField(default = uuid.uuid4, unique = True)
+    booking_in = models.DateField()
+    booking_out = models.DateField()
+    room_booked = models.ForeignKey(Room, on_deleted = models.CASCADE)
+
+    def __str__(self):
+        return self.booking_id
+
+class Update(models.Model):
+    title = models.CharField(max_length = 50)
+    image = models.ImageField(upload_to = 'updates')
+    description = RichTextField(blank = True)
+
+    def __str__(self) -> str:
+        return self.title
--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
+    services = Service.objects.all()[:3]
+    rooms = Room.objects.all()[:3]
+
+    dic = {
+        'services': services,
+        'rooms': rooms,
+    }
     return render(request, 'index.html')
